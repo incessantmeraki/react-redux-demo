09597f27126e02ecaefd85985ca58b1137464e7e
Ashwin Kandel <incessantmeraki@gmail.com>
2018-03-09 11:49:24 +1000

    Add tests for actions

--- src/App.test.js
+++ src/App.test.js
@@ -64,3 +64,68 @@ test('should set items to provided value', ()=>{
   const state = itemsReducer([], action)
   expect(state).toEqual(items)
 })
+
+/* -------------------------- Testing Actions --------------------------*/
+
+test('should generate itemsFetchError action object', ()=>{
+  const action = itemsFetchError(true)
+  expect(action).toEqual({
+    type:'ITEMS_FETCH_ERROR',
+    fetchError: true
+  })
+})
+
+test('should generate itemsLoading action object', ()=>{
+  const action = itemsLoading(true)
+  expect(action).toEqual({
+    type:'ITEMS_ARE_LOADING',
+    isLoading: true
+  })
+})
+
+test('should generate itemsFetchSucess action object', ()=>{
+  const items = ['one', 'two', 'three']
+  const action = itemsFetchSuccess(items)
+  expect(action).toEqual({
+    type:'ITEMS_FETCH_SUCCESS',
+    items: items
+  })
+})
+
+test('should successfully return data on API call', (done) => {
+  const store = createMockStore()
+  window.fetch = jest.fn().mockImplementation(()=>{
+    return Promise.resolve(
+      new window.Response('{"items": [1,2,3]}', {
+        status: 200,
+        statusText: 'Success',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+    )
+  })
+
+  return store.dispatch(startItemsFetch('http://test.com'))
+    .then(() => {
+      const expectedActions = store.getActions()
+      expect(expectedActions.length).toBe(3)
+      
+      expect(expectedActions[0]).toEqual({
+        type:'ITEMS_ARE_LOADING',
+        isLoading: true
+      })
+      expect(expectedActions[1]).toEqual({
+        type:'ITEMS_ARE_LOADING',
+        isLoading: false
+      })
+      expect(expectedActions[2]).toEqual({
+        type:'ITEMS_FETCH_SUCCESS',
+        items: {
+          items: [1,2,3]
+        }
+      })
+      
+      done()
+    })
+})
